857bb20e26836d2c8e7baacfb40d85399847df11
onexbash <fabian@schlegel.one>
2024-11-10 13:33:57 +0100

    ✨ logic to download & extract remote repositories
    will be saved in a tmp directory and removed after closing the buffer

--- lua/origin/init.lua
+++ lua/origin/init.lua
@@ -5,6 +5,18 @@ function M.setup(opts)
 
    local selected_provider
    local provider_base_url
+   local tmp_dir
+
+   if opts.tmp_dir then
+      tmp_dir = opts.tmp_dir
+   else
+      tmp_dir = "/tmp/onetmp"
+   end
+
+   if opts.custom_provider then
+      print("custom providers not supported yet" .. "defaulting to github")
+   end
+
    if opts.provider then
       print("provider set:" .. opts.provider)
       selected_provider = opts.provider
@@ -20,9 +32,11 @@ function M.setup(opts)
    elseif selected_provider == "bitbucket" then
       provider_base_url = "https://bitbucket.org"
    elseif selected_provider == "custom" then
-      print("custom providers not supported yet")
+      print("custom providers not supported yet" .. "defaulting to github")
+      provider_base_url = "https://github.com"
    else
-      print("unknown provider:" .. selected_provider)
+      print("unknown provider:" .. selected_provider .. "defaulting to github")
+      provider_base_url = "https://github.com"
    end
 
    vim.keymap.set("n", "<Leader>gh", function()
@@ -31,10 +45,14 @@ function M.setup(opts)
       local repo_name = repo_short_url:match("([^/]+)$")
       local repo_url = provider_base_url .. "/" .. repo_short_url
       local repo_install_url = repo_url .. "/archive/refs/heads/main.zip"
+      -- make sure tmp dir exists
       os.execute("mkdir -p /tmp/onetmp")
+      -- download repository as .zip
       os.execute(
          "wget -O /tmp/onetmp/" .. repo_name .. ".zip " .. repo_install_url
       )
+      -- extract .zip
+      os.execute("unzip -o " .. tmp_dir .. repo_name .. ".zip -d" .. tmp_dir)
    end)
 end
 
